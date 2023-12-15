713a7bade86d491feaa965e3ad225f531fc1de5c
doswald32 <dan.oswald32@gmail.com>
2023-12-15 14:37:18 -0700

    completed lab

--- index.js
+++ index.js
@@ -7,3 +7,23 @@ const returnLastTwoDrivers = function (array) {
     let newArray = array.slice(-2);
     return newArray;
 }
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(fare) {
+    const fareQuintupler = multiplier => fare * multiplier;
+    return fareQuintupler;
+}
+
+function fareDoubler(fare) {
+    return fare * 2;
+}
+
+function fareTripler(fare) {
+    return fare * 3;
+}
+
+function selectDifferentDrivers(arrayOfDrivers, drivers) {
+    let newArray = drivers(arrayOfDrivers);
+    return newArray;
+}
